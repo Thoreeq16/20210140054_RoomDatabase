77d0eb05980f97a682bf921afb78d1f4f6bdb682
Thoreeq16 <thoriqhidayatullah16@gmail.com>
2023-12-06 01:48:55 +0700

    filling the PenyediaViewModel.kt file

--- app/src/main/java/com/example/pertemuan10/model/PenyediaViewModel.kt
+++ app/src/main/java/com/example/pertemuan10/model/PenyediaViewModel.kt
@@ -1,2 +1,20 @@
 package com.example.pertemuan10.model
 
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.pertemuan10.AplikasiSiswa
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+
+        initializer {
+            EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+    }
+}
+fun CreationExtras.aplikasiSiswa(): AplikasiSiswa = (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
