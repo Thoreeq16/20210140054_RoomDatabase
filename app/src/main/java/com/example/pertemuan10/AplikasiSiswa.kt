31d9f05241041060acf2f2c4a6aea12624c0c08d
Thoreeq16 <thoriqhidayatullah16@gmail.com>
2023-12-06 01:40:14 +0700

    re-creating n fill the AplikasiSiswa file

--- app/src/main/java/com/example/pertemuan10/AplikasiSiswa.kt
+++ app/src/main/java/com/example/pertemuan10/AplikasiSiswa.kt
@@ -1,4 +1,14 @@
 package com.example.pertemuan10
 
-class AplikasiSiswa {
+import android.app.Application
+import com.example.pertemuan10.Repositori.ContainerApp
+import com.example.pertemuan10.Repositori.ContainerDataApp
+
+class AplikasiSiswa : Application() {
+    lateinit var container: ContainerApp
+
+    override fun onCreate() {
+        super.onCreate()
+        container = ContainerDataApp(this)
+    }
 }
