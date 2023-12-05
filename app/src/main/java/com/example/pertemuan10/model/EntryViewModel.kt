99febb601adabe5736f3aeefb828fce7b945c173
Thoreeq16 <thoriqhidayatullah16@gmail.com>
2023-12-06 00:28:11 +0700

    creating model package and EntryViewModel.kt, HomeViewModel.kt, PenyediaViewModel.kt files

---
+++ app/src/main/java/com/example/pertemuan10/model/EntryViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.pertemuan10.model
+
+import androidx.lifecycle.ViewModel
+import com.example.pertemuan10.Repositori.RepositoriSiswa
+
+class EntryViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel() {
+
+}
